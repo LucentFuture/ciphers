b56d465c8731afd8ad9207d755b4f450de78ad5a
Jimin L <69053105+chocothelee@users.noreply.github.com>
2021-04-13 23:12:48 +0800

    Create decoder.java
    - created class vingerecipher -> takes in variables ciphertext and key
    - created constructors (no blank constructor, must take in variables)
    - created method decoder (uses 2 variables: ciphertext and key)

---
+++ decoder.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+
+public class vingerecipher() {
+  private String ciphertext;
+  private String key;
+  
+  public cingerecipher(String ciphertext, String key) {
+    this.ciphertext = ciphertext;
+    this.key = key;
+  }
+  
+  /*
+  Pre-requisite: A ciphertext and key must be inputted
+  Post-requisite: No alterations to the ciphertext or key, returns a String with the decoded cipher
+  */
+  public String decoder(String ciphertext, String key) {
+    String keyLC = key.toLowerCase();
+    String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+  }
+  
+}
