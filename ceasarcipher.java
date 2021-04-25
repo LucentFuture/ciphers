ecbaed730d7f9d6dd8466044c9b0d2e1735a1486
Jimin L <69053105+chocothelee@users.noreply.github.com>
2021-04-25 23:46:25 +0800

    Update ceasarcipher.java
    - fixed key to be an integer, not a string
    - added more code for the decoder

--- ceasarcipher.java
+++ ceasarcipher.java
@@ -1,8 +1,8 @@
 public class ceasarcipher() implements cipher {
   private String ciphertext;
-  private String key;
+  private int key;
   
-  public ceasarcipher(String ciphertext, String key) {
+  public ceasarcipher(String ciphertext, int key) {
     this.ciphertext = ciphertext.toUpperCase();
     this.key = key.toUpperCase();
   }
@@ -11,7 +11,33 @@ public class ceasarcipher() implements cipher {
   Pre-requisite: A ciphertext and key must be inputed
   Post-requisite: No alterations to the ciphertext or key, returns a String with the decoded cipher in upper case
   */
-  public String decoder(String ciphertext, String key) {
+  public String decoder(String ciphertext, int key) {
+    String[] alphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
     
+    int shift = 25-key;
+    String newWord = "";
+    
+    for(int i=0; i<ciphertext.length(); i++) {
+      String cLet = ciphertext.charAt()+"";
+      
+      int cIndx;
+      int j=0;
+      while(j<alphabet.length){
+        if(alphabet[j].equals(cLet)) {
+          cIndx = q;
+          break;
+        }
+      }
+      
+      int newIndx = pIndx+key;
+      
+      while(newIndx>25) {
+        newIndx-=25;
+      }
+      newWord+=alphabet[newIndx];
+    }
+    
+    return newWord;
+  }
   }
 }
