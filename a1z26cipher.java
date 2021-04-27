d5c65042337c5e4d5a99d8921de3dfdfe694b2a2
Jimin L <69053105+chocothelee@users.noreply.github.com>
2021-04-27 23:49:22 +0800

    Update a1z26cipher.java
    - created cipher class that implements the cipher interface
    - added a constructor that takes a ciphertext as a String
    - created decoder method

--- a1z26cipher.java
+++ a1z26cipher.java
@@ -1,16 +1,14 @@
 public class a1z26cipher() implements cipher {
   private String ciphertext;
-  private int key;
   
-  public a1z26cipher(String ciphertext, int key) {
+  public a1z26cipher(String ciphertext) {
     this.ciphertext = ciphertext.toUpperCase();
-    this.key = key.toUpperCase();
   }
   
   /*
-  Pre-requisite: A ciphertext and key must be inputed
-  Post-requisite: No alterations to the ciphertext or key, returns a String with the decoded cipher in upper case
+  Pre-requisite: A ciphertext must be inputed as a String
+  Post-requisite: No alterations to the ciphertext, returns a String with the decoded cipher in upper case
   */
-  public String decoder(String ciphertext, int key) {
+  public String decoder(String ciphertext) {
     
   }
