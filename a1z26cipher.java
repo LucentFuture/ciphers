e8b13fc0cc1f33dc4235df8eba8e8322d37a447f
Jimin L <69053105+chocothelee@users.noreply.github.com>
2021-04-28 23:43:13 +0800

    Update a1z26cipher.java
    - added more information to the pre-requisite of the decoder method
    - added alphabet array into the decoder method

--- a1z26cipher.java
+++ a1z26cipher.java
@@ -6,9 +6,9 @@ public class a1z26cipher() implements cipher {
   }
   
   /*
-  Pre-requisite: A ciphertext must be inputed as a String
+  Pre-requisite: A ciphertext must be inputed as a String (ex: 8-5-12-12-15)
   Post-requisite: No alterations to the ciphertext, returns a String with the decoded cipher in upper case
   */
   public String decoder(String ciphertext) {
-    
+    String[] alphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
   }
