4e7c117bd0f6f9a6d11b2f9ef4c5bf1a03c1ce80
Jimin L <69053105+chocothelee@users.noreply.github.com>
2021-05-03 20:57:27 +0800

    Update a1z26cipher.java
    - wrote outline and pseudocode for how to solve the code

--- a1z26cipher.java
+++ a1z26cipher.java
@@ -11,4 +11,31 @@ public class a1z26cipher() implements cipher {
   */
   public String decoder(String ciphertext) {
     String[] alphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    String word = "";
+    
+    for(int i=0; i<ciphertext.length(); i++) {
+      String num = ciphertext.charAt(i)+"";
+      
+      if(i<ciphertext.length()-1) {
+        String next = ciphertext.charAt(i)+"";
+
+        if(num.equals("1") || num.equals("2")) {
+          //check next.equals("-")
+          //if next is a number, get said number
+        }
+- wro
+        if(next.equals("-")) {
+          //translate
+          i++;
+        }
+      }
+      else {
+        String prev = ciphertext.charAt(i-1)+"";
+        if(prev.equals("-")) {
+          //get letter
+        }
+      }
+      
+      return word;
+    }
   }
