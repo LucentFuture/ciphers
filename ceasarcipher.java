b1669e850567c3cb3aaa55a3d580de3142647666
Jimin L <69053105+chocothelee@users.noreply.github.com>
2021-06-05 21:09:56 +0900

    Update ceasarcipher.java
    - added prereq for initialization
    - fixed minor problems (typos, wrong variables, type of variable)

--- ceasarcipher.java
+++ ceasarcipher.java
@@ -1,43 +1,40 @@
-public class ceasarcipher() implements cipher {
+public class ceasar {
   private String ciphertext;
   private int key;
-  
-  public ceasarcipher(String ciphertext, int key) {
+	  
+  /* prerequisite: all text should be upper case */
+  public ceasar(String ciphertext, int key) {
     this.ciphertext = ciphertext.toUpperCase();
-    this.key = key.toUpperCase();
+    this.key = key;
   }
-  
-  /*
-  Pre-requisite: A ciphertext and key must be inputed
-  Post-requisite: No alterations to the ciphertext or key, returns a String with the decoded cipher in upper case
-  */
+	  
+  /* prerequisite: A ciphertext and key must be inputed
+  * post-requisite: No alterations to the ciphertext or key, returns a String with the decoded cipher in upper case */
   public String decoder(String ciphertext, int key) {
     String[] alphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-    
+
     int shift = 25-key;
     String newWord = "";
-    
+	    
     for(int i=0; i<ciphertext.length(); i++) {
-      String cLet = ciphertext.charAt()+"";
-      
+      String cLet = ciphertext.charAt(i)+"";
+
       int cIndx;
       int j=0;
       while(j<alphabet.length){
         if(alphabet[j].equals(cLet)) {
-          cIndx = q;
+          cIndx = j;
           break;
         }
       }
-      
-      int newIndx = pIndx+key;
-      
+    
+      int newIndx = cIndx+key;
+
       while(newIndx>25) {
         newIndx-=25;
       }
       newWord+=alphabet[newIndx];
     }
-    
     return newWord;
-  }
   }
 }
