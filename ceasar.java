553c7abddb65e247adaf1e2fca11da257615f8fe
Jimin L <69053105+chocothelee@users.noreply.github.com>
2021-06-06 22:38:12 +0900

    Update ceasar.java
    - changed decoder() to encoder() due to the code actually being an encoder
    - added proper decoder code

--- ceasar.java
+++ ceasar.java
@@ -8,13 +8,22 @@ public class ceasar {
 	}
 	
 	/* prerequisite: A message (to be decoded) and key must be inputed
-	* post-requisite: No alterations to the message or key, returns a String with the decoded cipher in upper case */
-	public String decoder() {
+	* post-requisite: No alterations to the message or key, returns a String with the encoded message in upper case */
+	public String encoder() {
 		String newmes = "";
 		for(int i=0; i<message.length(); i++) {
 			int let = (int)message.charAt(i);
 			newmes+=Character.toString((char)(let+key));
 		}
 		return newmes;
+	}
+	
+	public String decoder() {
+		String premes = "";
+		for(int i=0; i<message.length(); i++) {
+			int let = (int)message.charAt(i);
+			premes+=Character.toString((char)(let-key));
+		}
+		return premes;
 	}
 }
