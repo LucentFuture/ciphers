4afa5757011ce39345053fb215dd5e6d07bcf376
Jimin L <69053105+chocothelee@users.noreply.github.com>
2021-06-06 22:36:04 +0900

    Update and rename vigenerecipher.java to vigenere.java
    - changed name
    - added pseudo code to fix broken code

---
+++ vigenere.java
@@ -0,0 +1,21 @@
+public class vigenere {
+	private String message;
+	private String key;
+	
+	public vigenere(String message, String key) {
+		this.message = message.toUpperCase();
+	    this.key = key;
+	}
+	
+	/*
+  prerequisite: A message (to be decoded) and key must be inputed
+  postrequisite: No alterations to the ciphertext or key, returns a String with the decoded cipherin uppercase
+  */
+	public String decoder() {
+		//turn key to ascii, subtract values by 65
+    //subtract values accordingly to encode the message
+    //return decoded message
+		
+		return "";
+	}
+}
