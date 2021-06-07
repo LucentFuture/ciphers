95c217124ccfd31815d769839df7c8398b9e4666
Jimin L <69053105+chocothelee@users.noreply.github.com>
2021-06-07 22:52:03 +0900

    Update vigenere.java
    - updated format
    - added code (needs to still be worked on)
    - message is no longer inputted as uppercase

--- vigenere.java
+++ vigenere.java
@@ -1,21 +1,34 @@
+package ciphers;
+import java.util.ArrayList;
+
 public class vigenere {
 	private String message;
 	private String key;
 	
 	public vigenere(String message, String key) {
-		this.message = message.toUpperCase();
-	    this.key = key;
+		this.message = message;
+		this.key = key;
 	}
 	
-	/*
-  prerequisite: A message (to be decoded) and key must be inputed
-  postrequisite: No alterations to the ciphertext or key, returns a String with the decoded cipherin uppercase
-  */
+	/* prerequisite: A message (to be decoded) and key must be inputed
+	   * post-requisite: No alterations to the message or key, returns a String with the decoded cipher in upper case */
 	public String decoder() {
-		//turn key to ascii, subtract values by 65
-    //subtract values accordingly to encode the message
-    //return decoded message
+		ArrayList<Integer> keyarr = new ArrayList<Integer>();
 		
-		return "";
+		for(int j=0; j<key.length(); j++) {
+			keyarr.add((int)key.charAt(j));
+		}
+		
+		String newmes = "";
+		int j=0;
+		for(int i=0; i<message.length(); i++) {
+			int let = (int)message.charAt(i);
+
+			if(j==key.length()) {
+				j -= key.length();
+			}
+			newmes+=Character.toString((char)(let+keyarr.get(j)));
+	   	 }
+		return newmes;
 	}
 }
