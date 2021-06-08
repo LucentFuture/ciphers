96a4175fc7d127b1085bcde4f6afe76608aa11bd
Jimin L <69053105+chocothelee@users.noreply.github.com>
2021-06-08 22:21:53 +0900

    Update vigenere.java
    - finished encoder code (wasn't decoder)
    - changed decoder() into encoder()

--- vigenere.java
+++ vigenere.java
@@ -11,8 +11,8 @@ public class vigenere {
 	}
 	
 	/* prerequisite: A message (to be decoded) and key must be inputed
-	   * post-requisite: No alterations to the message or key, returns a String with the decoded cipher in upper case */
-	public String decoder() {
+	 * post-requisite: No alterations to the message or key, returns a String with the decoded cipher in upper case */
+	public String encoder() {
 		ArrayList<Integer> keyarr = new ArrayList<Integer>();
 		
 		for(int j=0; j<key.length(); j++) {
@@ -22,13 +22,15 @@ public class vigenere {
 		String newmes = "";
 		int j=0;
 		for(int i=0; i<message.length(); i++) {
-			int let = (int)message.charAt(i);
-
+	    		int let = (int)message.charAt(i);
+	    	
 			if(j==key.length()) {
 				j -= key.length();
 			}
-			newmes+=Character.toString((char)(let+keyarr.get(j)));
-	   	 }
+
+			newmes+=Character.toString((char)(let+(keyarr.get(j)-64)));
+			j++;
+		}
 		return newmes;
 	}
 }
