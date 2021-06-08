ddefbf068ba51b8bd4df72b516162705327bd6f4
Jimin L <69053105+chocothelee@users.noreply.github.com>
2021-06-08 22:22:58 +0900

    Update vigenere.java
    - added decoder() code
    - added prereq and postreq for decoder code

--- vigenere.java
+++ vigenere.java
@@ -11,7 +11,7 @@ public class vigenere {
 	}
 	
 	/* prerequisite: A message (to be decoded) and key must be inputed
-	 * post-requisite: No alterations to the message or key, returns a String with the decoded cipher in upper case */
+	 * postrequisite: No alterations to the message or key, returns a String with the decoded cipher in upper case */
 	public String encoder() {
 		ArrayList<Integer> keyarr = new ArrayList<Integer>();
 		
@@ -30,6 +30,30 @@ public class vigenere {
 
 			newmes+=Character.toString((char)(let+(keyarr.get(j)-64)));
 			j++;
+		}
+		return newmes;
+	}
+	
+	/* prerequisite: A message (to be decoded) and key must be inputed
+	 * postrequisite: No alterations to the message or key, returns a String with the decoded cipher in upper case */
+	public String decoder() {
+		ArrayList<Integer> keyarr = new ArrayList<Integer>();
+		
+		for(int j=0; j<key.length(); j++) {
+			keyarr.add((int)key.charAt(j));
+		}
+		
+		String newmes = "";
+		int j=0;
+		for(int i=0; i<message.length(); i++) {
+	    	int let = (int)message.charAt(i);
+	    	
+	    	if(j==key.length()) {
+	    		j -= key.length();
+	    	}
+	    	
+	    	newmes+=Character.toString((char)(let-(keyarr.get(j)-64)));
+	    	j++;
 		}
 		return newmes;
 	}
