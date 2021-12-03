1a2667cc98a78c4ab82bb0ea68b07e9a231a972c
Jimin L <69053105+chocothelee@users.noreply.github.com>
2021-12-03 22:04:40 +0700

    Update ascii.java
    - formatting update

--- ascii.java
+++ ascii.java
@@ -1,19 +1,19 @@
 public class ascii {
 	private int binnum;
-  private String letter;
-	
+	private String letter;
+
 	public ascii(int binnum, String letter) {
-    this.binnum = binnum;
-    this.letter = letter;
-	}
-  
-  /*input letter to convert to binary form*/
-	public String encoder() {
-    
+		this.binnum = binnum;
+		this.letter = letter;
 	}
 	
-  /*input binary form to convert to letter*/
+	/*input letter to convert to binary form*/
+	public String encoder() {
+		
+	}
+	
+	/*input binary form to convert to letter*/
 	public String decoder() {
-    
+		
 	}
 }
